b0b2552af44e6152193622c2f4e6079b5fc61d68
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2023-12-14 20:13:13 +0100

    Backend works

---
+++ backend/src/main/java/org/ericwubbo/shoppinglist/ShoppingListController.java
@@ -0,0 +1,20 @@
+package org.ericwubbo.shoppinglist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/shopping-list-items")
+public class ShoppingListController {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+                "boter",
+                "kaas",
+                "eieren"
+        );
+    }
+}
